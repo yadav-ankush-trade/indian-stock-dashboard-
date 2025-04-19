e27b01b47c55f2c768f5c2784591c2862a0dedd3
yadav-ankush-trade <ary.train12@gmail.com>
2025-04-19 17:00:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import streamlit as st
 import pandas as pd
-from utils.stock_utils import get_top_50_stocks
+from utils.stock_utils import get_nifty_500_symbols
 
-st.set_page_config(page_title="Top 50 Indian Stocks", layout="wide")
+
+
+st.set_page_config(page_title="Top 500 Indian Stocks", layout="wide")
 
 st.title("🏆 Top 50 Fundamentally Strong Indian Stocks")
 
-df = get_top_50_stocks()
+df = get_nifty_500_symbols()
 
 st.dataframe(df, use_container_width=True)
